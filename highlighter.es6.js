68fbcb01ff74753f73065d77388283b91e251926
Leroy Stav <roy@roomsinny.com>
2017-02-09 10:34:48 -0500

    border positioned correctly, moved event handlers into their own functions.  Added topic of conversation

--- highlighter.es6.js
+++ highlighter.es6.js
@@ -2,6 +2,7 @@
  * Created by Leroy on 2/5/2016.
  */
 
+// IIFE void function because this isn't fucking LISP
 void function (window, document, undefined) {
     "use strict";
 
@@ -11,7 +12,6 @@ void function (window, document, undefined) {
     // @border = @nindigreen @nindpurple
 
     const _window = window;
-    const tool = {noop, init, enableFinder, disableFinder, finder};
 
     const highlighter = (() => {
         let initialized = false;
@@ -45,35 +45,28 @@ void function (window, document, undefined) {
                      * 0 = n
                      * 1 = s
                      * 2 = e
-                     * r = w
+                     * 3 = w
                      **/
                     highlighter.border.forEach((edge, iedge, border) => {
                         edge.style.top = bounds.top + 'px';
                         edge.style.left = bounds.left + 'px';
                         edge.style.display = 'block';
                         edge.style.zIndex = 1 + (elem.zIndex || 0); // 1 + elem.z-index //TODO: fix me
-                        edge.style.zIndex = 20; // this seems to work.  why doesn't 1?
-                        //
-                        // switch (iedge) {
-                        //     case 0:
-                        //
-                        // }
+                        edge.style.zIndex = 20; // this seems to work.  why doesn't z=1?
 
-                        //TODO: check es6 spec. I think this should work
+                        // no longer TODO: check es6 spec. I think this should work
                         // this did work, I just got the cardinal directions wrong.  TODO: fix cardinal directions
                         switch (iedge) {
                             case 1: //s
-                                edge.style.left = bounds.left + bounds.width + 'px'; //TODO make border width configurable, place border properly
+                                edge.style.left = bounds.left + bounds.width - 4 + 'px'; //TODO make border width configurable
                             case 0: //n
                                 edge.style.height = bounds.height + 'px';
                                 break;
                             case 3: //w
-                                edge.style.top = bounds.top + bounds.height + 'px'; //TODO make border width configurable, place border properly
+                                edge.style.top = bounds.top + bounds.height - 4 + 'px'; //TODO make border width configurable
                             case 2: //e
                                 edge.style.width = bounds.width + 'px';
                         }
-
-                        console.log([edge.style.top, edge.style.left, edge.style.width, edge.style.height]);
                     })
 
 
@@ -88,13 +81,13 @@ void function (window, document, undefined) {
 
             // TODO: edge needs to be a css class as well
             highlighter.border.forEach(edge => {
-                edge.class="edge"
+                edge.class="edge";
                 edge.style.position = 'absolute';
                 edge.style.height = '4px'; //TODO make border width configurable
                 edge.style.width = '4px'; //TODO make border width configurable
                 edge.style.display = 'none';
                 edge.isEdge = true; //window.hasEdge = true; //there can be only one
-                edge.style.background = 'purple'; // TODO: make this configurable
+                edge.style.background = 'purple'; // TODO: make border color configurable
                 document.body.appendChild(edge);
             });
 
@@ -103,35 +96,34 @@ void function (window, document, undefined) {
 
     })();
 
+    const tool = {noop, init, enableFinder, disableFinder, finder};
 
     function noop() {
         console.log(arguments, 'noop');
-        debugger;
     }
 
+    //discuss amongst yourselves ... I'm just concerned about possible memory leaks, performance issues
+    //TODO: Should we remove keydown on execution and reattach on keyup?
+    //TODO: Same for keyup, reattach on keydown...
+    //TODO: mousemove only on finder on
+    //TODO: wheel --"--
     function init() {
         // enable tool when alt press on window
-        _window.addEventListener('keydown',
-            _keydown => _keydown.repeat || !_keydown.altKey || tool.enableFinder(),
-            true);
-
+        var _keydown = _e => _e.repeat || !_e.altKey || tool.enableFinder();
         // disable tool when alt released
-        _window.addEventListener('keyup',
-            _keyup => _keyup.keyCode === 18 && tool.disableFinder(),
-            true);
+        var _keyup  = _e => _e.keyCode === 18 && tool.disableFinder();
+        // find and highlight on mouse move
+        var _mousemove = () => tool.finder(_mousemove);
+        // traverse current DOM branch on wheel
+        var _wheel  = () => {
+            // tool.noop;
+        };
 
-        _window.addEventListener('mousemove',
-            _mousemove => tool.finder(_mousemove),
-            true);
+        _window.addEventListener('keydown', _keydown, true);
+        _window.addEventListener('keyup', _keyup, true);
+        _window.addEventListener('mousemove', _mousemove, true);
+        _window.addEventListener('wheel', _wheel, true);
 
-        
-
-        _window.addEventListener('wheel',
-            _wheel => {
-                // tool.noop;
-            },
-            true
-        )
     }
 
     function enableFinder() {
---
+++ tool.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Leroy on 12/1/2016.
+ */
+
+void function (window, document, undefined) {
+    "use strict";
+
+    //TODO: overlay highlight
+    // create less file.
+    // .edge()
+    // @border = @nindigreen @nindpurple
+
+    const _window = window;
+
+
+
+    const tool = {noop, init, enableFinder, disableFinder, finder};
+
+    tool.noop();
+
+    export tool;
+
+
+
+
+});
